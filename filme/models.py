539a0ad6c77c2d5b844c7d86c42f4eba1d879b88
caio-silva <caio_ceac23@hotmail.com>
2022-07-04 12:44:21 -0300

    aa

--- filme/models.py
+++ filme/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models heradsadasaaa
+# Create your models heradsadasaaaa
 
 LISTA_CATEGORIAS = (
     ("ANALISES", "Análises"),
