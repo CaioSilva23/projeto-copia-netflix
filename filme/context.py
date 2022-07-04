5252cedd65cc52673e64781fcbad7f62c8a73c7a
caio-silva <caio_ceac23@hotmail.com>
2022-07-04 15:28:32 -0300

    inicial

--- filme/context.py
+++ filme/context.py
@@ -2,10 +2,10 @@ from .models import Filme
 
 
 def lista_filmes_recentes(request):
-    lista_filmes = Filme.objects.all().order_by('-data_criacao')
+    lista_filmes = Filme.objects.all().order_by('-data_criacao')[0:8]
     return {"lista_filmes_recentes": lista_filmes}
 
 
 def lista_filmes_alta(request):
-    lista_alta = Filme.objects.all().order_by('-visualizacao')[0:10]
+    lista_alta = Filme.objects.all().order_by('-visualizacao')[0:8]
     return {'lista_filmes_alta': lista_alta}
